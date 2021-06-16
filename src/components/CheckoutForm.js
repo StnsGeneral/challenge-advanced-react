fe8e3039163e9d393d2e7a8b8d5feac174973aab
StnsGeneral <tristanmiller89@gmail.com>
2021-06-16 21:18:58 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/CheckoutForm.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { useForm } from '../hooks/useForm';
+
+const initialValue = {
+  firstName: '',
+  lastName: '',
+  address: '',
+  city: '',
+  state: '',
+  zip: '',
+};
+
+// This form should be handled by a "useForm" custom hook
+// Build out the logic needed for a form custom hook (see the useForm.js file)
+// and replace the necessary stateful logic from CheckoutForm with the hook
+
+const CheckoutForm = (props) => {
+  const [state, handleSubmit, handleChanges, showSuccessMessage] =
+    useForm(initialValue);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h2>Checkout Form</h2>
+        <label>
+          First Name:
+          <input
+            name="firstName"
+            value={state.firstName}
+            onChange={handleChanges}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            name="lastName"
+            value={state.lastName}
+            onChange={handleChanges}
+          />
+        </label>
+        <label>
+          Address:
+          <input
+            name="address"
+            value={state.address}
+            onChange={handleChanges}
+          />
+        </label>
+        <label>
+          City:
+          <input name="city" value={state.city} onChange={handleChanges} />
+        </label>
+        <label>
+          State:
+          <input name="state" value={state.state} onChange={handleChanges} />
+        </label>
+        <label>
+          Zip:
+          <input name="zip" value={state.zip} onChange={handleChanges} />
+        </label>
+        <button>Checkout</button>
+      </form>
+
+      {showSuccessMessage && (
+        <div className="success-message" data-testid="successMessage">
+          <p>
+            You have ordered some plants! Woo-hoo! <span role="img">🎉</span>
+          </p>
+          <p>Your new green friends will be shipped to:</p>
+          <br />
+          <br />
+          <p>
+            {state.firstName} {state.lastName}
+          </p>
+          <p>{state.address}</p>
+          <p>
+            {state.city}, {state.state} {state.zip}
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CheckoutForm;
